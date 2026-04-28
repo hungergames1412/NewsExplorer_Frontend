fa16283816a7069b2de8a9cc1062fc3a059a0bc1
Susan Hofmann <susan.hofmann@aol.com>
2026-04-27 19:47:14 -0500

    Add real News API key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const NEWS_API_KEY = "YOUR_NEWS_API_KEY";
+const NEWS_API_KEY = "bf237ba6099b4341922b8776dcae9bc7";
 const NEWS_API_URL = "https://newsapi.org/v2/everything";
 const NEWS_API_PROXY_URL = "https://nomoreparties.co/news/v2/everything";
 
